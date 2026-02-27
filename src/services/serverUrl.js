3aacbed10c16c519491bb6fc16e209a745ac7cd2
Yadu <yadukms2200@gmail.com>
2026-02-27 11:48:50 +0530

    resume-frontEnd

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,3 +1,4 @@
-const server_url='http://localhost:4000'
+// const server_url='http://localhost:4000'
+const server_url='https://resume-builder-backend-snu8.onrender.com'
 
 export default server_url;
